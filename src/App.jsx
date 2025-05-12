6e1308bb6835adf2e09121e4c64f458d68957cef
Bhanu Pratap Gandharv <bhanugandharv28@gmail.com>
2025-05-12 18:52:41 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './App.css'; // Add styles there
+import './App.css'; 
 
 const reviews = [
   {
